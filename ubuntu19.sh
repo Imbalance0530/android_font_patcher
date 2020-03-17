06fff93d1c32215510fcf0f2376ea4183ee6643d
JohnFawkes <33303104+JohnFawkes@users.noreply.github.com>
2020-03-17 14:42:05 -0400

    Add files via upload

--- ubuntu19.sh
+++ ubuntu19.sh
@@ -4,7 +4,7 @@ if [ -d "$folder" ]; then
 	first=1
 	echo "skipping downloading"
 fi
-tarball="ubuntu-rootfs.tar.xz"
+tarball="ubuntu-rootfs.tar.gz"
 
 if [ "$first" != 1 ];then
 	if [ ! -f $tarball ]; then
@@ -25,13 +25,13 @@ if [ "$first" != 1 ];then
 		*)
 			echo "unknown architecture"; exit 1 ;;
 		esac
-    wget "https://gitlab.com/JohnFawkes/termux-ubuntu/-/raw/master/Rootfs/Ubuntu/${arch}/ubuntu-rootfs-${arch}.tar.xz" -O $tarball
+    wget "https://partner-images.canonical.com/core/disco/current/ubuntu-disco-core-cloudimg-${arch}-root.tar.gz" -O $tarball
   fi
 	cur=`pwd`
 	mkdir -p "$folder"
 	cd "$folder"
 	echo "Decompressing Rootfs, please be patient."
-	proot --link2symlink tar -xJf ${cur}/${tarball} --exclude=dev||:
+	proot --link2symlink tar -xf ${cur}/${tarball} --exclude=dev||:
 	echo "fixing nameserver, otherwise it can't connect to the internet"
 	echo "nameserver 8.8.8.8" > etc/resolv.conf
 	echo "nameserver 8.8.4.4" > etc/resolv.conf
